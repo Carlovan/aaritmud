d37c4b1a09368542b986460fadd1f5d79e05f542
Giulio Carlassare <giulio.carlassare@gmail.com>
2016-08-03 16:40:43 +0200

    Now if log directory is not present is created when the program starts.

--- start.py
+++ start.py
@@ -10,6 +10,11 @@ Script minimale per avviare il gioco.
 #os.environ["PYCHECKER"] = "--stdlib"
 #import pychecker.checker
 
+import os.path
+
 if __name__ == "__main__":
+    if not os.path.isdir("log"):   #Create log dir if not present
+        os.mkdir("log")
+        print("Creata cartella log")
     from src.engine import engine
     engine.start()
